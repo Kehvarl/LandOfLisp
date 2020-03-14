7e3d467d39ffcd82425b48d6bed02adb57b3939e
Adam Corbett <kehvarl@gmail.com>
2020-03-14 12:50:31 -0700

    Start the Wizard Game text adventure

---
+++ wizard_game.lisp
@@ -0,0 +1,18 @@
+(defparameter *nodes* '((living-room (you are in the living room.
+                                      a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                                 there is a well in front of you.))
+                        (attic (you are in the attic.
+                                there is a giant welding torch in the corner.))))
+
+(defparameter *edges* '((living-room (garden west door)
+                         (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+  ;; flip/flop/flop.  Quasi-quoting to jump out of data mode.
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
