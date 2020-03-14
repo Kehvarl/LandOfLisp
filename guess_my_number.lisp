3f8bac40602a5b52946c177723158dbf3affbe45
Adam Corbett <kehvarl@gmail.com>
2020-03-14 11:02:50 -0700

    Updated game to track guessing statistics

--- guess_my_number.lisp
+++ guess_my_number.lisp
@@ -2,7 +2,10 @@
 
 (defparameter *big* 100)
 
+(defparameter *guesses* 0)
+
 (defun guess-my-number ()
+  (setf *guesses* (1+ *guesses*))
   (ash (+ *small* *big*) -1))
 
 (defun smaller ()
@@ -13,7 +16,11 @@
   (setf *small* (1+ (guess-my-number)))
   (guess-my-number))
 
+(defun correct ()
+  (format T "Guessed: ~a in ~a tries" (guess-my-number) *guesses*))
+
 (defun start-over ()
+  (defparameter *guesses* 0)
   (defparameter *small* 1)
   (defparameter *big* 100)
   (guess-my-number))
