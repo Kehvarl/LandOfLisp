47986459045c92ec0429f536d864155784a88d92
Adam Corbett <kehvarl@gmail.com>
2020-03-14 13:10:38 -0700

    Add in objects and object locating tools

--- wizard_game.lisp
+++ wizard_game.lisp
@@ -6,9 +6,16 @@
                                 there is a giant welding torch in the corner.))))
 
 (defparameter *edges* '((living-room (garden west door)
-                         (attic upstairs ladder))
+                                     (attic upstairs ladder))
                         (garden (living-room east door))
                         (attic (living-room downstairs ladder))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
 
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
@@ -20,3 +27,7 @@
 (defun describe-paths (location edges)
   (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+             (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
