d28d71f1d3bb05b0b8f0d4e22838a3964c23cd83
Adam Corbett <kehvarl@gmail.com>
2020-03-14 11:08:58 -0700

    Add some commentary on what each function is intended to do.

--- guess_my_number.lisp
+++ guess_my_number.lisp
@@ -1,24 +1,30 @@
+
 (defparameter *small* 1)
 
 (defparameter *big* 100)
 
 (defparameter *guesses* 0)
 
+;; Select the number between the two possible extremes
 (defun guess-my-number ()
   (setf *guesses* (1+ *guesses*))
   (ash (+ *small* *big*) -1))
 
+;; Guess was too high, set upper limit to 1 lower than the last guess
 (defun smaller ()
   (setf *big* (1- (guess-my-number)))
   (guess-my-number))
 
+;;  Guess was too low.  Set lower limit to 1 higher than last guess
 (defun bigger ()
   (setf *small* (1+ (guess-my-number)))
   (guess-my-number))
 
+;; Guessed the right number.  Print statistics
 (defun correct ()
   (format T "Guessed: ~a in ~a tries" (guess-my-number) *guesses*))
 
+;; Start a new game.  Initialize everything and make first guess.
 (defun start-over ()
   (defparameter *guesses* 0)
   (defparameter *small* 1)
