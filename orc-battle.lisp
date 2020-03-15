2442fc6ba5c90fc989e238b99d7c7f22fa16e8e3
Adam Corbett <kehvarl@gmail.com>
2020-03-15 13:30:44 -0700

    Core player functions

--- orc-battle.lisp
+++ orc-battle.lisp
@@ -8,6 +8,41 @@
 (defparameter *monster-builders* nil)
 (defparameter *monster-num* 12)
 
+(defun init-player()
+  (setf *player-health* 20)
+  (setf *player-agility* 30)
+  (setf *player-strength* 30))
+
+(defun player-dead-p ()
+  (<= *player-health* 0))
+
+(defun show-player ()
+  (fresh-line)
+  (princ "You are a valiant knight with a health of ")
+  (princ *player-health*)
+  (princ ", an agility of ")
+  (princ *player-agility*)
+  (princ ", and a strength of ")
+  (princ *player-strength*))
+
+(defun player-attack ()
+  (fresh-line)
+  (princ "Attack style: [s]tab [d]ouble swing [r]oundhouse:")
+  (case (read)
+    (s (monster-hit (pick-monster)
+                    (+ 2 (randval (ash *player-strength* -1)))))
+    (d (let ((x (randval (truncate (/ *player-strength* 6)))))
+         (princ "Your double swing has a strength of")
+         (print x)
+         (fresh-line)
+         (monster-hit (pick-monster) x)
+         (unless (monsters-dead-p)
+           (monster-hit (pick-monster) x))))
+    (otherwise (dotimes (x (1+ randval (truncate (/ *player-strength* 3))))
+                 (unless (monsters-dead-p)
+                   (monster-hit (random-monster) 1))))))
+
+
 (defun orc-battle ()
   (init-monsters)
   (init-player)
@@ -18,3 +53,16 @@
     (princ "Congratulations!  You have vanquished all your foes.")))
 
 
+(defun game-loop ()
+  (unless (or (platyer-dead-p) (monsters-dead-p))
+    (show-player)
+    (dotimes (k (1+ (truncate (/ (max 0 *player-agility*) 15 ))))
+      (unless (monsters-dead-p)
+        (show-monsters)
+        (player-attack)))
+    (fresh-line)
+    (map 'list
+         (lambda(m)
+           (or (monster-dead-p m) (monster-attack m)))
+         *monsters*)
+    (game-loop)))
