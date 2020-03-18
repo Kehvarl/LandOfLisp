12e4796d05bb6b0f666036c8d6fcf99405a54a08
Adam Corbett <kehvarl@gmail.com>
2020-03-17 22:07:03 -0700

    Inset slime mold

--- orc-battle.lisp
+++ orc-battle.lisp
@@ -60,6 +60,22 @@
     (princ " of its heads!  It also grows back one more head! ")
     (incf (monster-health m))
     (decf *player-health* x)))
+;; Slime Mold
+(defstruct (slime-mold (:include monster)) (sliminess (randval 5)))
+(push #'make-slime-mold *monster-builders*)
+(defmethod monster-show ((m slime-mold))
+  (princ "A slime mold with sliminess of ")
+  (princ (slime-mold-sliminess m)))
+(defmethod monster-attack ((m slime-mold))
+  (let ((x (randval (slime-mold-sliminess m))))
+    (princ "A slime mold wraps around your legs and decreases your agility by ")
+    (princ x)
+    (princ "! ")
+    (decf *player-agility* x)
+    (when (zerop (random 2))
+      (princ "It also squirts in your face, taking away a health point! ")
+      (decf *player-health*))))
+
 
 (defun init-player()
   (setf *player-health* 20)
