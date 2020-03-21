7824e68cf3c0efd2af5275ec82693a2994d5ab1d
Adam Corbett <kehvarl@gmail.com>
2020-03-20 18:26:42 -0700

    A simple database

---
+++ frobubase.lisp
@@ -0,0 +1,13 @@
+;;; Fobu-Base  Frob and Widget Database
+
+;;the clean, functional part
+(defun add-widget (database widget)
+  (cons widget database))
+
+;;the dirty, nonfunctional part
+(defparameter *database* nil)
+
+(defun main-loop()
+  (loop (princ "Please enter the name of a new widget:")
+        (setf *database* (add-widget *database* (read)))
+        (format t "The database contains the following: ~a%" *database*)))
