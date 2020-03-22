d8a47cc695e961e3674bf2922efa6ad383842a14
Adam Corbett <kehvarl@gmail.com>
2020-03-22 00:45:01 -0700

    Generate initial board

--- DiceOfDoom.lisp
+++ DiceOfDoom.lisp
@@ -3,3 +3,11 @@
 (defparameter *board-size* 2)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
 
+(defun board-array (lst)
+  (make-array *board-hexnum* :initial-contents lst))
+
+(defun gen-board ()
+  (board-array (loop for n below *board-hexnum*
+                     collect (list (random *numplayers*)
+                                   (1+ (random *max-dice*))))))
+
