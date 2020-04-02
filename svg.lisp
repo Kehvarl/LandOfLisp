6d93bcd617f4745973946a19e97cea3c67827d82
Adam Corbett <kehvarl@gmail.com>
2020-04-01 19:10:12 -0700

    Creating a DSL for making SVG graphics.

---
+++ svg.lisp
@@ -0,0 +1,9 @@
+(defun print-tag (name alst closingp)
+  (print #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+          (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+        alst)
+  (princ #\>))
