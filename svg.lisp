c153bf14a8ae2612dc94f1dbbcf65ea714534980
Adam Corbett <kehvarl@gmail.com>
2020-04-02 20:15:59 -0700

    Port in some functions from a previous chapter so we can build our TAG macro

--- svg.lisp
+++ svg.lisp
@@ -1,3 +1,36 @@
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+(defmacro let1 (var val &body body)
+  `(let ((,var ,val))
+     ,@body))
+
+(defmacro split (val yes no)
+  (let1 g (gensym)
+    `(let1 ,g ,val
+       (if ,g
+           (let ((head (car ,g))
+                 (tail (cdr ,g)))
+             ,yes)
+           ,no))))
+
+(defun pairs (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (if tail
+                        (f (cdr tail) (cons (cons head (car tail)) acc))
+                        (reverse acc))
+                    (reverse acc))))
+    (f lst nil)))
+
+
+
 (defun print-tag (name alst closingp)
   (print #\<)
   (when closingp
