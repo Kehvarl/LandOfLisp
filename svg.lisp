861a7a5f5dd61b7e92e644d501d8b9912b78e409
Adam Corbett <kehvarl@gmail.com>
2020-04-03 20:33:14 -0700

    Create a random graphlike polygon

--- svg.lisp
+++ svg.lisp
@@ -80,3 +80,24 @@
                                          (list (car tp) (cdr tp)))
                                        points))
                        style (svg-style color))))
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                       (1- length)))))
+
+(defun random-graph ()
+  (with-open-file (*standard-output* "random_walk.svg"
+                                     :direction :output
+                                     :if-exists :supersede)
+    (svg 400 200
+      (loop repeat 10
+            do (polygon (append '((0 . 200))
+                                (loop for x from 0 to 200
+                                      for y in (random-walk 100 400)
+                                      collect (cons x y))
+                                '((400 . 200)))
+                        (loop repeat 3
+                              collect (random 256)))))))
