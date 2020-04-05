cfc0c61973b0a89baedcaecd5f2be6d5330c7ef4
Adam Corbett <kehvarl@gmail.com>
2020-04-05 16:21:54 -0700

    Add weld command

--- wizard_game.lisp
+++ wizard_game.lisp
@@ -104,3 +104,20 @@
       (game-print (game-eval cmd))
       (game-repl))))
 
+(defun have (object)
+  (member object (cdr (inventory))))
+
+(defparameter *chain-welded* nil)
+
+(defun weld (subject object)
+  (if (and (eq *location* 'attic)
+           (eq subject 'chain)
+           (eq object 'bucket)
+           (have 'chain)
+           (have 'bucket)
+           (not *chain-welded*))
+      (progn (setf *chain-welded* t)
+             '(the chain is now securely welded to the bucket.))
+      '(you cannot weld like that.)))
+
+(pushnew 'weld *allowed-commands*)
