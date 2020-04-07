6848a39c8e522983d273b02ec4b8c4c3ae3bfc80
Adam Corbett <kehvarl@gmail.com>
2020-04-06 18:16:53 -0700

    Macro for game commands

--- wizard_game.lisp
+++ wizard_game.lisp
@@ -107,17 +107,14 @@
 (defun have (object)
   (member object (cdr (inventory))))
 
-(defparameter *chain-welded* nil)
+(defmacro game-action (command subj obj place &body body)
+  `(progn (defun ,command (subject object)
+            (if (and (eq *location* ',place)
+                     (eq subject ',subj)
+                     (eq object ',obj)
+                     (have ',subj))
+                ,@body
+                '(i can't ,command like that.)))
+          (pushnew ',command *alllowed-commands*)))
 
-(defun weld (subject object)
-  (if (and (eq *location* 'attic)
-           (eq subject 'chain)
-           (eq object 'bucket)
-           (have 'chain)
-           (have 'bucket)
-           (not *chain-welded*))
-      (progn (setf *chain-welded* t)
-             '(the chain is now securely welded to the bucket.))
-      '(you cannot weld like that.)))
 
-(pushnew 'weld *allowed-commands*)
